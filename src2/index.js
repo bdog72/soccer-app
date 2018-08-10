a53c4989f99ff630eb83159de4c0fb0d2705711c
Brian Bycynski <high5bri@hotmail.com>
2018-08-10 06:15:26 -0400

    finished lecture 45

--- src2/index.js
+++ src2/index.js
@@ -1,17 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './Resources/css/app.css';
-
-import { BrowserRouter } from 'react-router-dom';
 import Routes from './routes';
-
+import { BrowserRouter } from 'react-router-dom';
 const App = () => {
     return (
         <BrowserRouter>
-            <Routes/>
+            <Routes />
         </BrowserRouter>
-    )
-}
+    );
+};
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
